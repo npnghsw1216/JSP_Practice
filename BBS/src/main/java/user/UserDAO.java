730e01497b611d35b49f7ade0a83b02b3ba57bed
npnghsw1216 <npnghsw1216@naver.com>
2022-03-21 14:43:32 +0900

    데이터베이스 접근 객체 UserDAO생성

---
+++ BBS/src/main/java/user/UserDAO.java
@@ -0,0 +1,31 @@
+package user;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+public class UserDAO {
+	
+	private Connection conn; // 데이터베이스에 접근하게해주는 하니의 객체
+	private PreparedStatement pstmt;
+	private ResultSet rs; // 어떠한 정보를 담을 수 있는 하나의 객체
+	
+	// Ctrl+Shift+o 로 외부 라이브러리 추가
+	
+	// MySQL에 접속하게 해주는 부분
+	
+	// try/catch문을 이용하여 예외처리
+	
+	public UserDAO() {
+		try {
+			String dbURL = "jdbc:mysql://localhost:3306/BBS"; // local은 본인 컴퓨터로써 3306포트(MySQL주소)의 BBS에 접속
+			String dbID = "root"; // DB ID : DB ID 접속
+			String dbPassword = "root"; // DB Password : DB Password입력
+			Class.forName("com.mysql.jdbc.Driver"); // MySQLDriver : MySQL에 접속할 수 있도록 매게체역활을 해주는 하나의 라이브러리
+			conn = DriverManager.getConnection(dbURL, dbID, dbPassword); // 실질적으로 Driver를통해서 URL검색, 아이디 접속, 패스워드 접속
+		} catch(Exception e) { // 예외가 발생한 경우 해당예외를 출력
+			e.printStackTrace();
+		}
+	}
+}
